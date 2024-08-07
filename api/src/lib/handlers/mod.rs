41bfd38993256941197e1a3559ffa754025a3d85
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-08-06 20:50:09 -0700

    improvement: switch server to Axum

--- api/src/lib/handlers/mod.rs
+++
@@ -1,7 +0,0 @@
-// src/lib/handlers/mod.rs
-
-// module declarations
-pub mod health;
-
-// use declarations
-pub use health::*;
--- api/src/lib/lib.rs
+++
@@ -1,9 +0,0 @@
-// rivet-head-diary api
-// src/lib/lib.rs
-
-// module declarations
-pub mod handlers;
-pub mod startup;
-
-// use declarations
-pub use startup::*;
